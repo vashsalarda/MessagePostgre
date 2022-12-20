3567167d4e854438e644ecb2649272f02fc7ceee
Vash Salarda <vashful.v2@gmail.com>
2022-12-20 10:34:24 +0800

    Added Unit Tests

---
+++ Message.UnitTests/MessageControllerTests.cs
@@ -0,0 +1,14 @@
+using Message.API.Controllers;
+using Message.API.Entities;
+using Message.API.Repositories.Interfaces;
+
+namespace Message.UnitTests;
+
+public class MessageControllerUnitTest
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Message.UnitTests/Usings.cs
@@ -0,0 +1,3 @@
+global using Microsoft.AspNetCore.Mvc;
+global using Moq;
+global using Xunit;
